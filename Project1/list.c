1c7efe373eb031a087a7b0676d0613a4089416ad
Noor Syed <noorsyed1a@gmail.com>
2019-09-23 16:34:59 -0400

    finished warble, rle, started list

---
+++ Project1/list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct Node
+{
+    int value;
+    struct Node *next;
+};
+
+int main (int argc, char *argv[]){
+
+  char input[30];
+
+  if(input[0]=='i'){
+    //insert
+  }
+  else{
+    //delete
+  }
+
+}
---
+++ Project1/rle.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc, char *argv[]){
+  if(argc!=2){
+    printf("Enter a single argument.\n");
+    return 0;
+  }
+
+
+  int argSize = strlen(argv[1]);
+  printf("%d\t", argSize);
+  char arg[argSize];
+  strcpy(arg,argv[1]);
+  printf("%s\n", argv[1]);
+
+  char compress[argSize+1];
+  compress [argSize]='\0';
+
+  int i =0;
+  int j =0;
+  int compressPos = 0;
+
+  while(arg[i]!='\0'){
+    //int count = 0;
+    while((arg[j]!='\0')&&(arg[j]==arg[i])){
+
+      if(arg[j] >= '0' && arg[j] <= '9'){
+        printf("error\n");
+        return 0;
+      }
+
+      j++;
+      //TODO: check if j is a number
+    }
+
+    if(compressPos+1>argSize){
+      //printf("Original string is shorter\n");
+      printf("%s\n",argv[1]);
+      return 0;
+    }
+
+    compress[compressPos]=arg[i];
+    int numLetters = j-i;
+    compress[compressPos+1]=numLetters+'0';
+    compressPos=compressPos+2;
+    printf("%s\t",compress);
+    printf("%d\t",i);
+    printf("%d\n",j);
+    i=j;
+  }
+  compress[compressPos]='\0';
+  printf("Compressed String:\t");
+  printf("%s\n",compress);
+  //printf("%s\n", ;
+
+}
