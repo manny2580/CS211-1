aff9f5ddd532d7f86bd4acac145617beb511ae29
Noor Syed <noorsyed1a@gmail.com>
2019-09-25 01:15:57 -0400

    tried and failed to make a linked list

--- Project1/src/llist/llist.c
+++ Project1/src/llist/llist.c
@@ -11,61 +11,98 @@ struct Node{
 
 int main (int argc, char *argv[]){
 
-  struct Node *head = NULL;
-
-
-  FILE * file;
-  file = fopen (argv[1], "r");
-
-  char *input [30];
+  struct Node **head_ptr = NULL;
+  int length = 0;
 
   static char buffer[1024];
   while (fgets(buffer, 1024, stdin)){
 
-        printf("%s",buffer);
-        bool remove =isInsert(buffer[0]);
+        printf("%c",buffer[0]);
+        bool insert =isInsert(buffer[0]);
         buffer[0]=' ';
         int input =atoi(buffer);
-        printf("%d", input);
+        printf("%d\n", input);
 
-        if(buffer[0]=='i'){
+        if(insert){
           //insert
-          struct Node *ptr =  head;
+          printf("inserting..\n");
+          struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+          newNode->value = input;
+          newNode->next = NULL;
 
-          //create head
-          if(head==NULL){
-            head = (struct Node*)malloc(sizeof(struct Node));
-            head->value = input;
-            head->next = NULL;
+
+          //head is null
+          if(head_ptr==NULL){
+            printf("creating head..\n");
+            (*head_ptr) = newNode;
+            //printf("%d\n", head->value);
           }
+
+          //insert before head
+
+
+          struct Node *head = *head_ptr;
+
+          if(((struct Node *)*head_ptr)->value>input){
+            newNode->next = *head_ptr;
+            *head_ptr = newNode;
+          }
+
+          //insert in middle
           else{
-            struct Node *prev = ptr;
-            while(ptr->next!=NULL){
-              prev=ptr;
-              ptr = ptr->next;
-              if(ptr->value<input){
-                struct Node *newNode;
-                newNode = (struct Node*)malloc(sizeof(struct Node));
-                newNode->value = input;
-                prev->next = newNode;
-                newNode->next = ptr;
+            struct Node **ptr = head_ptr;
+            struct Node **prev = NULL;
+
+            while(((struct Node *)*ptr)->next!=NULL){
+
+              if(((struct Node *)*ptr)->value>input){
+                ((struct Node *)*prev)->next = newNode;
+                newNode->next = *ptr;
+                break;
               }
+              prev=ptr;
+              ptr= &(((struct Node *)ptr)->next);
             }
+
           }
+
+          //insert at end
+
+          struct Node *ptr = head;
+
+          while(ptr->next!=NULL){
+            printf("%d\t", ptr->value);
+          }
+          printf("\n");
+
+          length++;
+
+
       }
         else{
           //delete
         }
-
-
       }
-
-
 }
-
+//assumes chars are only i or d
 bool isInsert(char c){
   if(c=='i'){
     return true;
   }
   return false;
+}
+
+void push(struct Node** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+
+    /* 2. put in the data  */
+    new_node->value  = new_data;
+
+    /* 3. Make next of new node as head */
+    new_node->next = (*head_ref);
+
+    /* 4. move the head to point to the new node */
+    (*head_ref)    = new_node;
 }
